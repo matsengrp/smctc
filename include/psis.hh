09ecca189de09188bbffde672843fc00b5f549f7
Mathieu Fourment <m.fourment@gmail.com>
2018-10-31 18:07:53 +1100

    pareto smoothing importance sampling c++ implementation

---
+++ include/psis.hh
@@ -0,0 +1,14 @@
+//
+//  psis.hpp
+//  sts
+//
+//  Created by Mathieu Fourment on 8/10/2018.
+//  Copyright © 2018 Mathieu Fourment. All rights reserved.
+//
+
+#ifndef psis_hpp
+#define psis_hpp
+
+#include <stdio.h>
+
+#endif /* psis_hpp */
